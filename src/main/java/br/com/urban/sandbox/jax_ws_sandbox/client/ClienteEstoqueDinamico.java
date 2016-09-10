589d3a28fa8347177a4aad5d4f98944bd3ba33df
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-10 09:29:16 -0300

    creating a java client and testing correios ws

---
+++ src/main/java/br/com/urban/sandbox/jax_ws_sandbox/client/ClienteEstoqueDinamico.java
@@ -0,0 +1,36 @@
+package br.com.urban.sandbox.jax_ws_sandbox.client;
+
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import br.com.urban.sandbox.jax_ws_sandbox.model.item.Filtro;
+import br.com.urban.sandbox.jax_ws_sandbox.model.item.Item;
+import br.com.urban.sandbox.jax_ws_sandbox.model.item.TipoItem;
+import br.com.urban.sandbox.jax_ws_sandbox.ws.EstoqueWS;
+
+public class ClienteEstoqueDinamico {
+	
+	public static void main(String[] args) throws Exception {
+
+        URL url = new URL("http://localhost:8080/estoquews?wsdl");
+        QName qname = new QName("http://ws.estoque.caelum.com.br/", "EstoqueWSService");
+
+        Service service = Service.create(url, qname);
+
+        EstoqueWS cliente = service.getPort(EstoqueWS.class);
+
+        Filtro filtro = new Filtro();
+        filtro.setNome("IPhone");
+        filtro.setTipo(TipoItem.CELULAR);;
+
+        List<Item> itens = cliente.getItens();
+
+        for (Item item : itens) {
+            System.out.println(item.getNome());
+        }
+    }
+
+}
---
+++ src/main/java/br/com/urban/sandbox/jax_ws_sandbox/correios/soap/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.com.urban.sandbox.jax_ws_sandbox.correios.soap;
