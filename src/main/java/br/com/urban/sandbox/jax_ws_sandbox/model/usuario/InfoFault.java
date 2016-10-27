6e2de3aff8ccbf22022737af74d17838b418326f
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-10-27 19:50:36 -0200

    removing warnings

--- src/main/java/br/com/urban/sandbox/jax_ws_sandbox/model/usuario/InfoFault.java
+++ src/main/java/br/com/urban/sandbox/jax_ws_sandbox/model/usuario/InfoFault.java
@@ -8,16 +8,24 @@ import javax.xml.bind.annotation.XmlAccessorType;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class InfoFault {
 
-    private Date dataErro;
-    private String mensagem;
+	private Date dataErro;
+	private String mensagem;
 
-    public InfoFault(String mensagem, Date dataErro) {
-        this.mensagem = mensagem;
-        this.dataErro = dataErro;
-    }
+	public InfoFault(String mensagem, Date dataErro) {
+		this.mensagem = mensagem;
+		this.dataErro = dataErro;
+	}
 
-    //JAX-B precisa
-    InfoFault() {
-    }
+	public Date getDataErro() {
+		return dataErro;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	// JAX-B precisa
+	InfoFault() {
+	}
 
 }
